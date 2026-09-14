73bed0d7d798e59d17fcc8ba40cd5e1c010a454d
admytruk79-netizen <admytruk79@gmail.com>
2026-09-14 16:48:11 -0700

    Load refined Hidden Laws theme

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -3,7 +3,9 @@ export async function onRequest(context){
   const url=new URL(context.request.url);
   if(url.pathname!=='/'||!response.headers.get('content-type')?.includes('text/html'))return response;
   const html=await response.text();
-  const injected=html.replace('</body>','<script src="/dynamic-content.js" defer></script></body>');
+  const withTheme=html.replace('</head>','<link rel="stylesheet" href="/theme.css?v=20260914"></head>');
+  const injected=withTheme.replace('</body>','<script src="/dynamic-content.js?v=20260914" defer></script></body>');
   const headers=new Headers(response.headers);headers.delete('content-length');
+  headers.set('cache-control','no-store');
   return new Response(injected,{status:response.status,statusText:response.statusText,headers});
 }
