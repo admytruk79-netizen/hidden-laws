d1e9fdd3872d55202a5838845bb7d28a96f517b8
admytruk79-netizen <admytruk79@gmail.com>
2026-09-14 18:43:15 -0700

    Load ASCEND status enhancements on public homepage

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,7 +2,10 @@ export async function onRequest(context){
   const response=await context.next();
   const url=new URL(context.request.url);
   if(url.pathname!=='/'||!response.headers.get('content-type')?.includes('text/html'))return response;
+  const html=await response.text();
+  const injected=html.replace('</body>','<script src="/site-enhancements.js?v=20260914-2" defer></script></body>');
   const headers=new Headers(response.headers);
+  headers.delete('content-length');
   headers.set('cache-control','no-store');
-  return new Response(response.body,{status:response.status,statusText:response.statusText,headers});
+  return new Response(injected,{status:response.status,statusText:response.statusText,headers});
 }
