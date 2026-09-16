ce07ac0de4690022f7df7fd8d629b392f3baccf5
admytruk79-netizen <admytruk79@gmail.com>
2026-09-16 11:20:14 -0700

    Load artwork quality guard

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -31,6 +31,9 @@ export async function onRequest(context){
   if(!injected.includes('/visual-layout-fix.js')){
     injected=injected.replace('</body>','<script src="/visual-layout-fix.js?v=20260916-1" defer></script></body>');
   }
+  if(!injected.includes('/image-quality-fix.js')){
+    injected=injected.replace('</body>','<script src="/image-quality-fix.js?v=20260916-1" defer></script></body>');
+  }
   const headers=new Headers(response.headers);
   headers.delete('content-length');
   headers.set('cache-control','no-store');
