ac1593a772e899ae6ee2d220ba1ca9792dbad3e0
admytruk79-netizen <admytruk79@gmail.com>
2026-09-16 11:15:50 -0700

    Load final artwork sizing fix

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -28,6 +28,9 @@ export async function onRequest(context){
   if(!injected.includes('/lineage-display-fix.js')){
     injected=injected.replace('</body>','<script src="/lineage-display-fix.js?v=20260916-1" defer></script></body>');
   }
+  if(!injected.includes('/visual-layout-fix.js')){
+    injected=injected.replace('</body>','<script src="/visual-layout-fix.js?v=20260916-1" defer></script></body>');
+  }
   const headers=new Headers(response.headers);
   headers.delete('content-length');
   headers.set('cache-control','no-store');
