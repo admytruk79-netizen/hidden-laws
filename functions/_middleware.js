85272431bdf494b30efa17f1dbe1f8a48bceb9b7
admytruk79-netizen <admytruk79@gmail.com>
2026-09-15 18:17:14 -0700

    Load fixed book links

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -19,6 +19,9 @@ export async function onRequest(context){
   if(!injected.includes('/art-motion-fix.js')){
     injected=injected.replace('</body>','<script src="/art-motion-fix.js?v=20260915-1" defer></script></body>');
   }
+  if(!injected.includes('/book-links.js')){
+    injected=injected.replace('</body>','<script src="/book-links.js?v=20260915-1" defer></script></body>');
+  }
   const headers=new Headers(response.headers);
   headers.delete('content-length');
   headers.set('cache-control','no-store');
