3a5ecb782e75ef1c0b62830a49adffab295488db
admytruk79-netizen <admytruk79@gmail.com>
2026-09-16 11:14:17 -0700

    Load lineage display repair

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -25,6 +25,9 @@ export async function onRequest(context){
   if(!injected.includes('/typography-stability.js')){
     injected=injected.replace('</body>','<script src="/typography-stability.js?v=20260915-1" defer></script></body>');
   }
+  if(!injected.includes('/lineage-display-fix.js')){
+    injected=injected.replace('</body>','<script src="/lineage-display-fix.js?v=20260916-1" defer></script></body>');
+  }
   const headers=new Headers(response.headers);
   headers.delete('content-length');
   headers.set('cache-control','no-store');
