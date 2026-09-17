fcd975cc33534ee83a2cd7e535af7ad603523f23
admytruk79-netizen <admytruk79@gmail.com>
2026-09-16 17:34:35 -0700

    Load high-resolution lineage image replacements

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -34,6 +34,9 @@ export async function onRequest(context){
   if(!injected.includes('/image-quality-fix.js')){
     injected=injected.replace('</body>','<script src="/image-quality-fix.js?v=20260916-1" defer></script></body>');
   }
+  if(!injected.includes('/lineage-image-quality.js')){
+    injected=injected.replace('</body>','<script src="/lineage-image-quality.js?v=20260916-1" defer></script></body>');
+  }
   const headers=new Headers(response.headers);
   headers.delete('content-length');
   headers.set('cache-control','no-store');
